455ebd6311a0a502b9772f4fbb085e09a5f2dbc7
Rohit Yadav <rohit221d@gmail.com>
2024-10-18 21:13:13 +0530

    Calculator Project

---
+++ script.js
@@ -0,0 +1,23 @@
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+    const display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+}
+
+function appendEle(chara) {
+    const display = document.getElementById("display");
+    display.value = display.value + chara;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "error!";
+    }
+}
